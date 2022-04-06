358d05e7995d02023f22ddacae746dc555cb1286
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2022-04-06 16:22:55 -0400

    Added labeler usage

--- myscript.sh
+++ myscript.sh
@@ -7,16 +7,23 @@ SCAP=".scap";
 
 
 FILENAME=$(echo "$NAME" | cut -f 1 -d '.');
-#echo $FILENAME
+export FILENAME
+echo $FILENAME
 #python svg-to-scap.py input/$NAME -o $1.scap
 # python svg-to-scap.py input/$1 -o $1.scap
 scapfile="$FILENAME$SCAP"
 #echo ${scapfile}
 
-
+longname="\"1\": [\"data/$FILENAME"
+echo $longname
 python svg_to_scap.py input/$NAME -o output/$scapfile
 
 
 cd ../../labeler
 echo $(pwd)
-sed
+
+# the following line changes the only filename present in inputs.js
+# to the current file (entered through terminal)
+perl -i -pe 's/"1":.?\["[a-z]\w+\/[a-zA-z]\w+/$ENV{longname}/' inputs.js
+open http://localhost:8000
+python -m http.server

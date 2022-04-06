55bbb9947e02b564aa20a1cf3a3522130784b8c4
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2022-04-06 09:09:47 -0400

    Till Labeller
    -need to add a way to edit inputs.js file and open the simple http

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
-cd strokeaggregator
-pwd
+cd strokeaggregator/core;
+NAME="$1";
+SCAP=".scap";
+#echo ${NAME};
+
+
+FILENAME=$(echo "$NAME" | cut -f 1 -d '.');
+#echo $FILENAME
+#python svg-to-scap.py input/$NAME -o $1.scap
+# python svg-to-scap.py input/$1 -o $1.scap
+scapfile="$FILENAME$SCAP"
+#echo ${scapfile}
+
+
+python svg_to_scap.py input/$NAME -o output/$scapfile
+
+
+cd ../../labeler
+echo $(pwd)
+sed
